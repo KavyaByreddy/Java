eb3c112951db6878ccd30eb3cc1df5e2cd9064c8
KavyaByreddy <kavyabyreddy68@gmail.com>
2023-07-07 17:19:55 +0530

    Java

---
+++ TernaryMax.java
@@ -0,0 +1,7 @@
+public class TernaryMax{
+    public static void main(String[] args){
+        int num1=500,num2=1000,max;
+        max=(num1>num2) ? num1 : num2;
+        System.out.println(max);
+    }
+}
---
+++ Ternaryoperator.java
@@ -0,0 +1,8 @@
+class employee{
+    public static void main(String[] args)
+    {
+        int emp_salary=40000;
+        String result = emp_salary>40000?"eligible for marriage":"not eligible";
+        System.out.println(result);
+    }
+}
